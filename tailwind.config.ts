75a9ce517f596b1df1c0e58babae9926ca189460
Ethan Webber <eswebber07@gmail.com>
2024-12-12 16:12:06 +0000

    i LOVE tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,6 @@
 import type { Config } from "tailwindcss";
 
-const disabledCss = {
-  'code::before': false,
-  'code::after': false,
-  'blockquote p:first-of-type::before': false,
-  'blockquote p:last-of-type::after': false,
-  pre: false,
-  code: false,
-  'pre code': false,
-  'code::before': false,
-  'code::after': false,
-}
+
 
 const config: Config = {
   darkMode: 'class',
@@ -24,13 +14,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      typography: {
-        DEFAULT: { css: disabledCss },
-        sm: { css: disabledCss },
-        lg: { css: disabledCss },
-        xl: { css: disabledCss },
-        '2xl': { css: disabledCss },
-      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
